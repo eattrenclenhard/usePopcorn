1750b3a3dd6a9551311d7838f53727287169a07b
Eat Tren Clen Hard <125723190+eattrenclenhard@users.noreply.github.com>
2025-03-12 18:07:16 +0800

    Define public API to allow customizations -rating scale -color -size -custom class name -custom set of text labels -default/initial rating -make rating value available outside the component

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,30 @@ import { createRoot } from "react-dom/client";
 // import "./index.css";
 // import App from './App.jsx'
 import StarRating from "./StarRating";
+import { useState } from "react";
+
+function ThirdPartyStar() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      {/* allow user to specify handler function/pass as prop */}
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
     <StarRating maxRating={10} />
+    <StarRating
+      size={24}
+      color="maroon"
+      className="test"
+      messages={["Terrible", "Bad", "Mediocre", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <ThirdPartyStar />
   </StrictMode>
 );
