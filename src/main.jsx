35d129e8fb7b6decb50bce84e26a061d082cedf9
Eat Tren Clen Hard <125723190+eattrenclenhard@users.noreply.github.com>
2025-03-13 01:04:42 +0800

    Restrict data type that would result in feature breaking from being passed into the component using 'proptype/typechecking'

--- src/main.jsx
+++ src/main.jsx
@@ -19,7 +19,7 @@ function ThirdPartyStar() {
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={10} />
+    <StarRating maxRating={5} />
     <StarRating
       size={24}
       color="maroon"
