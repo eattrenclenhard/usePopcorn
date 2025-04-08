b9792783e32b0df7660bdafe74af92fd8d9e7949
Eat Tren Clen Hard <125723190+eattrenclenhard@users.noreply.github.com>
2025-04-08 22:17:39 +0800

    Clean up data fetching, optimize performance, cut off unnecessary fetch request

--- src/App.jsx
+++ src/App.jsx
@@ -77,12 +77,15 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok)
@@ -90,9 +93,12 @@ export default function App() {
           const data = await res.json();
           if (data.Response === "False") throw new Error("Movie not found");
           setMovies(data.Search);
+          setError("");
         } catch (err) {
           console.error(err.message);
-          setError(err.message);
+          if (err.className !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -105,6 +111,10 @@ export default function App() {
       }
 
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
@@ -317,6 +327,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     function () {
       if (!title) return;
       document.title = `Movie | ${title}`;
+
       return function () {
         document.title = "usePopcorn";
         console.log(`clean up effect for movie ${title}`);
